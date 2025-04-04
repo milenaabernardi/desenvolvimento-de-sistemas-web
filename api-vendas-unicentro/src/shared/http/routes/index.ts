9242395baddec8fcb088d13774b604754c98ba43
milena <milenaabernardi@gmail.com>
2025-04-04 20:36:08 -0300

    atividade em sala

---
+++ api-vendas-unicentro/src/shared/http/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const routes = Router();
+
+routes.get('/', (request, response) =>{
+    response.json({message: 'Hello Dev!'});
+    return;
+})
+
+export default routes;
